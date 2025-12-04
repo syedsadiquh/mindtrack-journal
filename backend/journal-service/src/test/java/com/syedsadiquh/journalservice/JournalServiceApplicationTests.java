4fcae36f57f16443b946269b63bf51b9a7b999d3
Syed Sadiqu Hussain <syedsadiquh@gmail.com>
2025-12-04 22:45:42 +0530

    init all services

---
+++ backend/journal-service/src/test/java/com/syedsadiquh/journalservice/JournalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syedsadiquh.journalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
