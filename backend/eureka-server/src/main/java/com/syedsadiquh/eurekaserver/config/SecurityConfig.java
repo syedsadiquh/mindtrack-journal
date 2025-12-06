fa71981743407c8e375769faee4634008ca043c9
Syed Sadiqu Hussain <syedsadiquh@gmail.com>
2025-12-06 19:11:58 +0530

    fix(csrf): enable csrf only for UI dashboard

--- backend/eureka-server/src/main/java/com/syedsadiquh/eurekaserver/config/SecurityConfig.java
+++ backend/eureka-server/src/main/java/com/syedsadiquh/eurekaserver/config/SecurityConfig.java
@@ -4,16 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
-                .csrf(customizer -> customizer.disable())
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/eureka/**"))
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
