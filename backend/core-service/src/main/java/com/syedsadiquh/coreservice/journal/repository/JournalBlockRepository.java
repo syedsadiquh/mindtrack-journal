ce199e8a62dd192b740ad8a16a98273f161a1458
Syed Sadiqu Hussain <syedsadiquh@gmail.com>
2026-04-10 00:36:00 +0530

    feat(journal: added journal pages and block logic and endpoints

--- backend/core-service/src/main/java/com/syedsadiquh/coreservice/journal/repository/JournalBlockRepository.java
+++ backend/core-service/src/main/java/com/syedsadiquh/coreservice/journal/repository/JournalBlockRepository.java
@@ -1,7 +1,6 @@
 package com.syedsadiquh.coreservice.journal.repository;
 
 import com.syedsadiquh.coreservice.journal.entity.JournalBlock;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
