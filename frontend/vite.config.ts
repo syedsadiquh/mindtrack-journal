ce86be777de28c1875ec131418a2d4db84e0b26a
Syed Sadiqu Hussain <syedsadiquh@gmail.com>
2026-04-19 23:24:10 +0530

    feat(vercel): vercel deploy settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,18 @@
-import { defineConfig, type ConfigEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-export default async ({ command }: ConfigEnv) => {
-  const isBuild = command === "build";
-  const plugins = [tanstackStart(), react(), tailwindcss(), tsconfigPaths()];
-
-  if (isBuild) {
-    const { cloudflare } = await import("@cloudflare/vite-plugin");
-    plugins.push(cloudflare());
-  }
-
-  return defineConfig({
-    plugins,
-    server: {
-      host: "::",
-      port: 3000,
-    },
-  });
-};
+export default defineConfig({
+  plugins: [
+    tanstackStart({ spa: { enabled: true } }),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+  server: {
+    host: "::",
+    port: 3000,
+  },
+});
