48c63f6c7194ac8a30b24c4917b1b041c057d5d4
Syed Sadiqu Hussain <syedsadiquh@gmail.com>
2026-04-20 02:41:26 +0530

    fix(profile): profile update

--- backend/core-service/src/main/java/com/syedsadiquh/coreservice/user/dto/request/UpdateUserRequestDto.java
+++ backend/core-service/src/main/java/com/syedsadiquh/coreservice/user/dto/request/UpdateUserRequestDto.java
@@ -1,10 +1,14 @@
 package com.syedsadiquh.coreservice.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateUserRequestDto {
     private String fullName;
     private String username;
